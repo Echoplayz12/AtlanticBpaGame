520f73f8ab7404dcb577ebece9f334c87eff3004
PS25BMagar <162139688+PS25BMagar@users.noreply.github.com>
2024-09-18 14:07:23 -0400

    commit-BM

---
+++ Assets/Script/PlayerMovement/PlayerMovement.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [Header("References")]
+    public Rigidbody rb;
+    public Transform head;
+    public Camera cameraPos;
+
+    [Header("Configuration")]
+    public float walkSpeed;
+    public float runSpeed;
+    public float jumpForce;
+    public float impactThreshold;
+
+    [Header("Runtime")]
+    Vector3 newVelocity;
+    bool isGrounded = false;
+    bool isJumping = false;
+    bool inWater = false;
+    float vyCache;
+
+    //incase we want a score
+    private int count;
+    public TextMeshProUGUI countText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rigidbody call
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * 2f);
+
+        newVelocity = Vector3.up * rb.velocity.y;
+        //if input key-left shift- can run, else walk
+        float speed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
+        newVelocity.x = Input.GetAxis("Horizontal") * speed;
+        newVelocity.z = Input.GetAxis("Vertical") * speed;
+
+        //jumping
+        if(isGrounded)
+        {
+
+        }
+    }
+}

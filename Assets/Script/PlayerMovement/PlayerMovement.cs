eada047dd75cece2fe6587071a2a4fd5a3aa2987
PS25BMagar <162139688+PS25BMagar@users.noreply.github.com>
2024-09-20 13:13:29 -0400

    commit 9/20/24

--- Assets/Script/PlayerMovement/PlayerMovement.cs
+++ Assets/Script/PlayerMovement/PlayerMovement.cs
@@ -4,70 +4,71 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    [Header("References")]
-    public Rigidbody rb;
-    public Transform head;
-    public Camera cameraPos;
-
-    [Header("Configuration")]
+    [Header("Movement")]
     public float walkSpeed;
-
     public float runSpeed;
+    public float gDrag;
 
     public float startYScale;
     public float crouchSpeed;
     public float crouchYScale;
-   
-    public float jumpForce;
 
+    public float jumpForce;
     public float impactThreshold;
 
-    [Header("Runtime")]
-    Vector3 newVelocity;
-    bool isGrounded = false;
-    bool isJumping = false;
-    bool inWater = false;
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
 
+
+    public Transform body;
+
+    float horizantalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+
+    [Header("Runtime")]
+    bool isGrounded;
+    bool isJumping;
+    bool inWater;
     float vyCache;
 
-    //incase we want a score
-    //private int count;
-    //public TextMeshProUGUI countText;
 
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-
-        //rigidbody call
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
-
-        startYScale = transform.localScale.y;
     }
-
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * 2f);
+        ///ground check
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+        
+        PlyerInput();
 
-        newVelocity = Vector3.up * rb.velocity.y;
-        //if input key-left shift- can run, else walk
-        float speed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
-        newVelocity.x = Input.GetAxis("Horizontal") * speed;
-        newVelocity.z = Input.GetAxis("Vertical") * speed;
 
-        //jumping
-        if(isGrounded)
+        //drag handler
+        if (isGrounded)
         {
+            rb.drag = gDrag;
+            //jumping condition
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 rb.velocity = new Vector3(rb.velocity.x, 1f, rb.velocity.z);
                 rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
             }
+        }
+        else
+        {
+            rb.drag = 0;
         }
         //crouching
         if (Input.GetKeyDown(KeyCode.LeftControl))
@@ -82,61 +83,23 @@ public class NewBehaviourScript : MonoBehaviour
             transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
         }
     }
-    void FixedUpdate()
-    {
-        rb.velocity = transform.TransformDirection(newVelocity);
-        vyCache = rb.velocity.y;
-    }
-    void LateUpdate()
-    {
-        //vertical rotation
-        Vector3 e = head.eulerAngles;
-        e.x -= Input.GetAxis("Mouse Y") * 2f;
-        e.x = RestrictAngle(e.x, -90, 90f);
-        head.eulerAngles = e;
-    }
-    public static float RestrictAngle(float angle, float angleMin, float angleMax)
-    {
-        //clamp the vertical head roation/ I could of used Mathf.Clamp but didn't 
-        if (angle > 180)
-            angle -= 360;
-        else if (angle < -180)
-            angle += 360;
-        if (angle > angleMax)
-            angle = angleMax;
-        if (angle < angleMin)
-            angle = angleMin;
-        return angle;
-    }
-    void OnCollisionStay(Collision collision)
-    {
-        isGrounded = true;
-        isJumping = false;
-    }
-    void OnCollisionExit(Collision collision)
-    {
-        isGrounded = false;
-    }
-    void OnCollisionEnter(Collision collision)
-    {   // movment 
-        if (Vector3.Dot(collision.GetContact(0).normal, Vector3.up) < .5f)
-        {
-            if (rb.velocity.y < -5f)
-            {
-                rb.velocity = Vector3.up * rb.velocity.y;
-                return;
-            }
-        }
 
-        float acceleration = (rb.velocity.y - vyCache) / Time.fixedDeltaTime;
-        float impactForce = rb.mass * Mathf.Abs(acceleration);
-
-        if (impactForce >= impactThreshold)
-        {
-            SceneManager.LoadScene(1);
-            Cursor.visible = true;
-            Cursor.lockState = CursorLockMode.Confined;
-        }
+    private void FixedUpdate()
+    {
+        MovePlayer();
     }
 
+    private void PlyerInput()
+    {
+        horizantalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+    }
+
+    private void MovePlayer()
+    {
+        moveDirection = body.forward * verticalInput + body.right * horizantalInput;
+
+        rb.AddForce(moveDirection.normalized * walkSpeed * 10f, ForceMode.Force);
+    }
 }

cefcfeb691e10cd85f7d165b8510f220d74271f0
MKRuin117 <143616644+MKRuin117@users.noreply.github.com>
2024-10-04 13:03:41 -0400

    Update _profile_.cs
    10/4/24

--- Assets/Script/SaveScripts/_profile_.cs
+++ Assets/Script/SaveScripts/_profile_.cs
@@ -4,5 +4,25 @@ using UnityEngine;
 
 class Profile{
     string profile_name = "Profile_1";
-
+    int artifacts_collected = 0;
+    int logs_collected = 0;
+    bool selected_profile = false;
 }
+class Artifact{
+    string artifact_name = "Artifact";
+    int artifact_id = 1;
+    bool log_attached = false;
+}
+class Log{
+    int log_number = 1;
+    string log_text = "*Log Text";
+    int hidden_value = 0;
+}
+
+    static void Main(string[] args){
+        Profile profile_1 = new Profile();
+        Profile profile_2 = new Profile();
+        Profile profile_3 = new Profile();
+
+        
+    }

7891b67f8003e25692c1c272dcb0a5c28c98cce4
PS25BMagar <162139688+PS25BMagar@users.noreply.github.com>
2024-10-21 11:31:34 -0400

    comit

--- Assets/Script/PlayerMovement/MovingOb.cs
+++ Assets/Script/PlayerMovement/MovingOb.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class MovingOb : MonoBehaviour
 {
-    public Rigidbody rb;
     public float speed = 3;
-    public Vector3 coord = new Vector3(4, 1, 1);
+    public float smoothTime = 0.5f;
+    public Vector3 coord = new Vector3(1, 1, 4);
+    Vector3 currentVelocity;
 
     void Update()
     {
         //transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.World);
-        transform.position = Vector3.SmoothDamp(transform.position, coord, ref ;
+        transform.position = Vector3.SmoothDamp(transform.position, coord, ref currentVelocity, smoothTime;
     }
 }

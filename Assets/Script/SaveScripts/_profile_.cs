02a1e0a1cb9a5e4ad07755b2fe5eb515986e590e
PS25BMagar <162139688+PS25BMagar@users.noreply.github.com>
2024-11-12 12:37:05 -0500

    commit

--- Assets/Script/SaveScripts/_profile_.cs
+++ Assets/Script/SaveScripts/_profile_.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-class Profile
+class Profile: MonoBehaviour
 {
     //Fields of the profile class to be use in the object of which will be used in the save system
     //The profile contains all save data to be put in a psuedo CSV file 
-    
-   private string profile_name;
-   private int num_artifacts_collected = 0;
-   private int[] artifacts_collected = {};
-   private int num_logs_collected = 0;
-   private int[] logs_collected = {};
-   private bool selected_profile = false;
+
+    private string profile_name;
+    private int num_artifacts_collected = 0;
+    private int[] artifacts_collected = { };
+    private int num_logs_collected = 0;
+    private int[] logs_collected = { };
+    private bool selected_profile = false;
     //Profile object constructor
     public Profile()
     {
@@ -23,24 +23,31 @@ class Profile
         logs_collected = logs_collected;
         selected_profile = false;
     }
-
+   public string Profile_Name
+    {
+        get { return profile_name; }
+        set { profile_name = profile_name; }
     public string Profile_Name{
         get {return profile_name;}
         set {profile_name = profile_name;}
     }
-    public int Num_Artifacts_Collected
-    {
+    public int Num_Artifacts_Collected{
         get { return num_artifacts_collected; }
-        set { num_artifacts_collected = num_artifacts_collected + 1; }
+        set { num_artifacts_collected = num_artifacts_collected++; }
     }
-    public int[] Artifacts_Collected{
-        get {return artifacts_collected;}
-        set {artifacts_collected[num_artifacts_collected] = 0;}
+    public int[] Artifacts_Collected
+    {
+        get { return artifacts_collected; }
+        set { artifacts_collected[num_artifacts_collected] = 0; }
+    }
+    public int Num_Logs_Collected{
+        get { return num_logs_collected;}
+        set {num_logs_collected++;}
     }
 
 }
-
-public class Pickup
+//General PickUp class of which the artifacts and logs are derived from
+public class Pickup: MonoBehaviour
 {
     string pickup_name = "";
     int pickup_id = 0;
@@ -50,31 +57,36 @@ public class Pickup
 
     }
 }
-public class Artifact
+//Artifact class which holds the artifact id which are put in the profile object
+public class Artifact: MonoBehaviour
 {
     string artifact_name = "Artifact";
     public int artifact_id = 1;
     bool log_attached = false;
 
-    public Artifact(){
-        artifact_name=artifact_name;
+    public Artifact()
+    {
+        artifact_name = artifact_name;
         artifact_id = artifact_id;
         log_attached = log_attached;
     }
-    
-    public int Artifact_ID{
-        get {return artifact_id;}
-        set {artifact_id = artifact_id+1;}
+
+    public int Artifact_ID
+    {
+        get { return artifact_id; }
+        set { artifact_id = artifact_id + 1; }
     }
 }
-public class Log
+//Log class which holds the log id which are put in the profile object
+
+public class Log: MonoBehaviour
 {
     int log_number = 1;
     string log_text = "*Log Text";
     int hidden_value = 0;
 }
 
-public class SaveSystem : MonoBehavior
+/*public class SaveSystem : MonoBehavior
 {
 
-}
+}*/
